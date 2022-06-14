5e1e07cad307ac7680f3c3f17d7329ca7f771ffa
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-06-14 11:44:09 -0300

    Service Class

--- src/main/java/tech/getarrays/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/tech/getarrays/employeemanager/repo/EmployeeRepo.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.getarrays.employeemanager.model.Employee;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+  void deleteEmployeeById(Long id);
+  Optional<Employee> findEmployeeById(Long id);
 }
